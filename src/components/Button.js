8ab891b271aaaee397b929f0b9f0a24ae48ce90d
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-10-23 16:08:33 +0500

    use styled components with gatsby

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styled from "styled-components"
+
+export const Button = styled.button`
+  background: green;
+  color: white;
+  font-style: 2rem;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { LayoutWrapper } from "../components"
+import { Button } from "../components/Button"
 
 export default function Home() {
   return (
     <LayoutWrapper>
-      <>Hello world!</>
+      <>
+        <h1>Hello world!</h1>
+
+        <Button>Styled button</Button>
+      </>
     </LayoutWrapper>
   )
 }
