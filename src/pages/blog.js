907660823eb57f0385477dd6a00ba156fea4a3d9
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-10-23 12:32:41 +0500

    use css modules for styling benefit of modules is it localizes stylesheet classes for a single file so you don't have to come up with unique class names for different files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
 import { LayoutWrapper } from "../components"
+import styles from "./blog.module.css"
 
 export default function blog() {
   return (
     <LayoutWrapper>
       <div>This is blog page</div>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor
+        vel massa non mattis. Nullam eu enim quis turpis blandit vehicula sed in
+        ante. Nunc et leo nibh. Nulla eros enim, gravida sed hendrerit eu,
+        rhoncus in orci. Nulla euismod posuere accumsan. Donec fringilla nulla
+        in urna scelerisque, gravida luctus velit volutpat. Sed at commodo
+        metus. Cras lobortis non nunc in pulvinar. Vivamus finibus molestie
+        elementum. Nunc tincidunt dolor at tincidunt sollicitudin. Sed risus
+        nulla, rhoncus eu mauris ac, tempus mollis mi.
+      </p>
     </LayoutWrapper>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,9 +1,20 @@
 import React from "react"
 import { LayoutWrapper } from "../components"
+import styles from "./products.module.css"
 export default function products() {
   return (
     <LayoutWrapper>
-      <div>Products</div>
+      <h1>Products</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor
+        vel massa non mattis. Nullam eu enim quis turpis blandit vehicula sed in
+        ante. Nunc et leo nibh. Nulla eros enim, gravida sed hendrerit eu,
+        rhoncus in orci. Nulla euismod posuere accumsan. Donec fringilla nulla
+        in urna scelerisque, gravida luctus velit volutpat. Sed at commodo
+        metus. Cras lobortis non nunc in pulvinar. Vivamus finibus molestie
+        elementum. Nunc tincidunt dolor at tincidunt sollicitudin. Sed risus
+        nulla, rhoncus eu mauris ac, tempus mollis mi.
+      </p>
     </LayoutWrapper>
   )
 }
